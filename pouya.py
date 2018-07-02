c71b42a5813e2a466c61a0f980ab451081bc63a3
Leon <leon.jarabek@rocketmail.com>
2018-07-02 11:23:22 +0200

    tests with spectral embedded outputs

--- pouya.py
+++ pouya.py
@@ -1,6 +1,7 @@
 import scipy.io as sio
 import numpy as np
 from pprint import pprint
+from sklearn.manifold import SpectralEmbedding
 import matplotlib.pyplot as plt
 import configparser
 from preprocessing import reject_outliers_and_standardize
@@ -11,6 +12,7 @@ from preprocessing import reject_outliers_and_standardize
 config = configparser.ConfigParser()
 config.read('config.ini')
 cfg = config['DEFAULT']
+files_path = cfg['files_path']
 BatchSize = int(cfg['batchsize'])
 TrainMaxIndex = int(cfg['trainMaxIndex'])
 testFrom = int(cfg['testFrom'])
@@ -19,28 +21,28 @@ path = cfg['MAT_path']
 
 data = sio.loadmat(path)
 
-X_train = data['X_train']
-Y_train = data['Y_train']
-X_pred = data['X_pred']
-
-features = Y_train.shape[1]
+#X_train = data['X_train']
+#Y_train = data['Y_train']
+#X_pred = data['X_pred']
+#
+#features = Y_train.shape[1]
 
 #print(X_train.shape)
 #print(X_pred.shape)
 #print(Y_train.shape)
 
-X_mean = np.mean(X_train)
-X_std = np.std(X_train)
-Y_mean = np.mean(Y_train)
-Y_std = np.std(Y_train)
+#X_mean = np.mean(X_train)
+#X_std = np.std(X_train)
+#Y_mean = np.mean(Y_train)
+#Y_std = np.std(Y_train)
 
 
-X_train = (X_train - X_mean) / (np.std(X_train) + 0.000001)
-Y_train = (Y_train - Y_mean) / (Y_std + 0.000001)
+#X_train = (X_train - X_mean) / (np.std(X_train) + 0.000001)
+#Y_train = (Y_train - Y_mean) / (Y_std + 0.000001)
 #plt.imshow(X_train[4])
 #plt.show()
 
-def saveResults(results, savedir, no_features = features, size = BatchSize):
+"""def saveResults(results, savedir, no_features = features, size = BatchSize):
     #Y = np.load(resultsDir) #resultsDir C:/PouyaResults/Y_pred.npy'
     Y = results[:,0,:,:]
     Y_new = np.zeros(shape=(X_pred.shape[0],no_features))
@@ -57,20 +59,62 @@ def saveResults(results, savedir, no_features = features, size = BatchSize):
     pprint(Y_new.shape)
     #plt.plot(Y_new[:,3])
     #plt.show()
-    """plt.plot(data['Y_train'])
-    plt.show()
-    plt.plot(Y_new[:,0])
-    plt.show()
-    plt.plot(Y_new[:, 1])
-    plt.show()
-    plt.plot(Y_new[:, 2])
-    plt.show()
-    plt.plot(Y_new[:, 3])
-    plt.show()"""
-    return Y_new
+    
+    return Y_new"""
+
+def preprocess():
+    data1 = sio.loadmat(files_path + 'fx1_XTRAIN.mat')
+    data2 = sio.loadmat(files_path + 'Y_train.mat')
+
+    X_train = data1['X_TRAIN']  # (110, 80, 80)
+
+    Y_train = data2['Y_train']  # (110, 1024, 768, 1, 3)
 
 
+    try:
+        Y = np.load(files_path+"Y.npy")
+        print("loaded Y from memory")
+    except IOError:
+        Z = np.zeros((110, 1024, 768, 2), dtype = np.float32)
+        print("standardizing and saving Y")
+        for idz, z in enumerate(Y_train):
+            Z[idz,:,:,0] = np.transpose(np.transpose(z[:,:,0,0]) - np.arange(1024))
+            Z[idz,:,:,1] = z[:,:,0,1] - np.arange(768)
+        Y = Z/np.std(Z)
+        del Z
+        np.save(files_path+"Y.npy",Y)
+    try:
+        Y = np.load(files_path+"Y_se.npy")
+        print("loaded SE from memory")
+    except IOError:
+        Y = np.reshape(Y, (Y.shape[0], Y.shape[1] * Y.shape[2] * Y.shape[3]))
+        Y = SpectralEmbedding(6).fit_transform(X=Y)
+        np.save(files_path+"Y_se.npy", Y)
+        print("generated&saved SE")
+    try:
+        X = np.load(files_path+"X.npy")
+        print("loaded X from memory")
+    except IOError:
+        print("standardizing and saving X")
+        X = (X_train[:,23:39, 20:43] - np.mean(X_train[:,23:39, 20:43])) / np.std(X_train[:,23:39, 20:43])
+        np.save(files_path + "X.npy", X)
 
+    try:
+        X_pred = np.load(files_path+"X_pred.npy")
+        print("loaded X_pred from memory")
+    except IOError:
+        print("normalising and saving x_pred")
+        X_pred = data1['X_PRED']
+        X_pred = (X_pred[:,23:39, 20:43] - np.mean(X_train[:,23:39, 20:43])) / np.std(X_train[:,23:39, 20:43])
+        np.save(files_path+"X_pred.npy",X_pred)
+
+    return X, Y, X_pred #clear clutter from memory
+
+X_train, Y_train, X_pred = preprocess()
+#plt.imshow(X_train[50])
+#plt.show()
+#plt.hist(Y_train)
+#plt.show()
 def getBatch(size = BatchSize, maxsize = X_train.shape[0], minsize = 0):
     maxsize = maxsize - size
     rn = np.random.randint(0, maxsize+1-minsize, size = (size))
@@ -79,16 +123,16 @@ def getBatch(size = BatchSize, maxsize = X_train.shape[0], minsize = 0):
     for i in rn:
         arr.append(X_train[i])
         coordinates.append(Y_train[i])
-    return np.array(arr), coordinates
+    return np.array(arr), np.array(coordinates)
 
 
 def getValBatch(size = BatchSize, maxsize = X_train.shape[0]):
     minsize = maxsize - size
-    rn = np.arange(minsize,maxsize)
+    #rn = np.arange(minsize,maxsize)
     arr = []
     coordinates = []
-    for i in rn:
+    for i in np.arange(minsize,maxsize): # in rn
         arr.append(X_train[i])
         coordinates.append(Y_train[i])
-    return np.array(arr), coordinates
+    return np.array(arr), np.array(coordinates)
 
--- pouyaModel.py
+++ pouyaModel.py
@@ -12,15 +12,12 @@ import scipy.io as sio
 import os
 import re as re
 
-
-
-
-
 # THE MODEL - wide residual network*
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 cfg = config['DEFAULT']
+files_path = cfg['files_path']
 BatchSize = int(cfg['batchsize'])
 TrainMaxIndex = int(cfg['trainMaxIndex'])
 testFrom = int(cfg['testFrom'])
@@ -30,63 +27,69 @@ path = cfg['MAT_path']
 regularization = float(cfg['regularization'])
 
 data = sio.loadmat(path)
+# Y = np.load(files_path + "Y.npy")
+# X_train = data['X_train']
+# Y_train = data['Y_train']
+#X_pred = data['X_PRED']
+# X_pred = (X_pred - np.mean(X_train)) / (np.std(X_train) + 0.000001)
 
-X_train = data['X_train']
-Y_train = data['Y_train']
-X_pred = data['X_pred']
-X_pred = (X_pred - np.mean(X_train)) / (np.std(X_train) + 0.000001)
-#cfgCrop = config['CROP']
-#yFrom = int(cfgCrop['yFrom'])
-#yTo = int(cfgCrop['yTo'])
+# cfgCrop = config['CROP']
+# yFrom = int(cfgCrop['yFrom'])
+# yTo = int(cfgCrop['yTo'])
 
+X_train, Y_train, X_pred = CT.preprocess()
 
 seed = 42
 
-input, answer = CT.getBatch()   # placeholders
-epoch = X_train.shape[0]
-features = Y_train.shape[1]
+input, answer = CT.getBatch()  # placeholders
+epoch = input.shape[0]
+features = answer.shape[1]
+
+print(str(input.shape) + " X shape")
+print(str(answer.shape) + " Y shape")
 input = np.array(input)
 input = np.expand_dims(input, 3)
 
 print('INPUT SHAPE: {}'.format(input.shape))
 
+# MAKE directory for results:
 
-#MAKE directory for results:
 
-
-SaveTo = 'c:/PouyaResults/' + "regularization_" + str(regularization) + "batch_" + str(BatchSize) + re.findall(r'(fx[0-9])',path)[0] + "_" + str(time()) + '/'
+SaveTo = 'c:/PouyaResults/' + "regularization_" + str(regularization) + "batch_" + str(BatchSize) + \
+         re.findall(r'(fx[0-9])', path)[0] + "_" + str(time()) + '/'
 if not os.path.exists(SaveTo):
     os.makedirs(SaveTo)
     os.makedirs(SaveTo + "/y_pred/")
 
-
 phase_train_bool = False
 phase_train = tf.placeholder(tf.bool, name='phase_train')
-y = tf.placeholder(tf.float32, shape=[BatchSize,features])  # coordinate input
-x = tf.placeholder(tf.float32, shape =[BatchSize, len(input[0]), len(input[0,0]), 1])  # surface input
+y = tf.placeholder(tf.float32, shape=[BatchSize, features])  # coordinate input
+x = tf.placeholder(tf.float32, shape=[BatchSize, len(input[0]), len(input[0, 0]), 1])  # surface input
 with tf.name_scope('conv2d_1'):
-     w1 = tf.get_variable(name = 'W1', shape = [3,3,1,16], initializer=xavier_initializer(uniform = True, seed=seed), regularizer=l2_regularizer(regularization))
-     conv1 = tf.nn.conv2d(x,w1, strides = [1,1,1,1], padding = 'VALID')
-     conv1  = layers.batch_norm(conv1,16, phase_train)
-     act1 = tf.nn.relu(conv1)
+    w1 = tf.get_variable(name='W1', shape=[3, 3, 1, 16], initializer=xavier_initializer(uniform=True, seed=seed),
+                         regularizer=l2_regularizer(regularization))
+    conv1 = tf.nn.conv2d(x, w1, strides=[1, 1, 1, 1], padding='VALID')
+    conv1 = layers.batch_norm(conv1, 16, phase_train)
+    act1 = tf.nn.relu(conv1)
 
 with tf.name_scope('BLOCK1'):
-    w2 = tf.get_variable(name='W2', shape= [3,3,16,32], initializer=xavier_initializer(uniform=True, seed=seed),
+    w2 = tf.get_variable(name='W2', shape=[3, 3, 16, 32], initializer=xavier_initializer(uniform=True, seed=seed),
                          regularizer=l2_regularizer(regularization))
 
-    w21 = tf.get_variable(name='W2.1', shape= [1,1,16,32], initializer=xavier_initializer(uniform=True, seed=seed),
-                         regularizer=l2_regularizer(regularization))
+    w21 = tf.get_variable(name='W2.1', shape=[1, 1, 16, 32], initializer=xavier_initializer(uniform=True, seed=seed),
+                          regularizer=l2_regularizer(regularization))
     conv2 = tf.nn.conv2d(act1, w2, strides=[1, 1, 1, 1], padding='SAME')
     conv2 = layers.batch_norm(conv2, 32, phase_train)
     conv2 = tf.nn.relu(conv2)
     w22 = tf.get_variable(name='W21', shape=[3, 3, 32, 32], initializer=xavier_initializer(uniform=True, seed=seed),
-                         regularizer=l2_regularizer(regularization))
+                          regularizer=l2_regularizer(regularization))
     conv2 = tf.nn.conv2d(conv2, w22, strides=[1, 1, 1, 1], padding='SAME')
-    conv21 = tf.nn.conv2d(act1, w21, strides=[1, 1, 1, 1], padding='SAME') #!! 19.4.2018: strides 1111 -> 1221 ???? *results were obtained with 1,1,1,1
+    conv21 = tf.nn.conv2d(act1, w21, strides=[1, 1, 1, 1],
+                          padding='SAME')  # !! 19.4.2018: strides 1111 -> 1221 ???? *results were obtained with 1,1,1,1
     act3 = conv2 + conv21
 
 with tf.name_scope('BLOCK2'):
-    conv3  = layers.batch_norm(act3, 32, phase_train)
+    conv3 = layers.batch_norm(act3, 32, phase_train)
     conv3 = tf.nn.relu(conv3)
     w3 = tf.get_variable(name='W3', shape=[3, 3, 32, 32], initializer=xavier_initializer(uniform=True, seed=seed),
                          regularizer=l2_regularizer(regularization))
@@ -94,10 +97,9 @@ with tf.name_scope('BLOCK2'):
     conv3 = layers.batch_norm(conv3, 32, phase_train)
     conv3 = tf.nn.relu(conv3)
     w31 = tf.get_variable(name='W31', shape=[3, 3, 32, 32], initializer=xavier_initializer(uniform=True, seed=seed),
-                         regularizer=l2_regularizer(regularization))
+                          regularizer=l2_regularizer(regularization))
     conv3 = tf.nn.conv2d(conv3, w31, strides=[1, 1, 1, 1], padding='SAME')
     act4 = conv3 + act3
-
 
 act4 = layers.batch_norm(act4, 32, phase_train)
 act4 = tf.nn.relu(act4)
@@ -109,10 +111,10 @@ with tf.name_scope('BLOCK3'):
     conv4 = layers.batch_norm(conv4, 64, phase_train)
     conv4 = tf.nn.relu(conv4)
     w41 = tf.get_variable(name='W41', shape=[3, 3, 64, 64], initializer=xavier_initializer(uniform=True, seed=seed),
-                         regularizer=l2_regularizer(regularization))
+                          regularizer=l2_regularizer(regularization))
     conv4 = tf.nn.conv2d(conv4, w41, strides=[1, 1, 1, 1], padding='SAME')
     w42 = tf.get_variable(name='W4.2', shape=[1, 1, 32, 64], initializer=xavier_initializer(uniform=True, seed=seed),
-                         regularizer=l2_regularizer(regularization))
+                          regularizer=l2_regularizer(regularization))
     conv41 = tf.nn.conv2d(act4, w42, strides=[1, 2, 2, 1], padding='SAME')
     act5 = conv41 + conv4
 
@@ -145,7 +147,6 @@ with tf.name_scope('BLOCK5'):
     conv61 = tf.nn.conv2d(act6, w62, strides=[1, 2, 2, 1], padding='SAME')
     act7 = conv61 + conv6
 with tf.name_scope('BLOCK6'):
-
     conv7 = layers.batch_norm(act7, 128, phase_train)
     conv7 = tf.nn.relu(conv7)
     w7 = tf.get_variable(name='W7', shape=[3, 3, 128, 128], initializer=xavier_initializer(uniform=True, seed=seed),
@@ -159,128 +160,121 @@ with tf.name_scope('BLOCK6'):
     act8 = conv7 + act7
 with tf.name_scope('BLOCK_X'):
     act8 = layers.batch_norm(act8, 128, phase_train)
-    act8  = tf.nn.relu(act8)
-    #act8 = tf.nn.avg_pool(act8, ksize=[1, 8, 8, 1], strides =[1,1,1,1], padding = 'VALID') #Keep it valid - keep it real
-
+    act8 = tf.nn.relu(act8)
+    # act8 = tf.nn.avg_pool(act8, ksize=[1, 8, 8, 1], strides =[1,1,1,1], padding = 'VALID') #Keep it valid - keep it real
 
 with tf.name_scope('FC_1'):
-    #act = fully_connected(act1,50, activation_fn=tf.nn.relu)
-    #wfc = tf.get_variable(name= 'w_FC_1', shape = [97,140,64], initializer=xavier_initializer(uniform=True, seed = seed), regularizer=l2_regularizer(regularization)) #size in 64 --> size out 5
-    #act = tf.scan(lambda x: tf.matmul(x,wfc, transpose_a=True), act1)
+    # act = fully_connected(act1,50, activation_fn=tf.nn.relu)
+    # wfc = tf.get_variable(name= 'w_FC_1', shape = [97,140,64], initializer=xavier_initializer(uniform=True, seed = seed), regularizer=l2_regularizer(regularization)) #size in 64 --> size out 5
+    # act = tf.scan(lambda x: tf.matmul(x,wfc, transpose_a=True), act1)
     flat = flatten(act8)
     activationFC1 = tf.layers.dense(
-    flat,
-    128, #20!
-    activation=None,
-    use_bias=False,
-    kernel_initializer=xavier_initializer(uniform=True, seed=seed),
-    #kernel_initializer=tf.initializers.truncated_normal(mean = 0, stddev= 0.001),
-    bias_initializer=tf.constant_initializer(0.1),
-    kernel_regularizer=l2_regularizer(regularization),
-    bias_regularizer=None,
-    activity_regularizer=None,
-    trainable=True,
-    name=None,
-    reuse=None
-)
+        flat,
+        128,  # 20!
+        activation=None,
+        use_bias=False,
+        kernel_initializer=xavier_initializer(uniform=True, seed=seed),
+        # kernel_initializer=tf.initializers.truncated_normal(mean = 0, stddev= 0.001),
+        bias_initializer=tf.constant_initializer(0.1),
+        kernel_regularizer=l2_regularizer(regularization),
+        bias_regularizer=None,
+        activity_regularizer=None,
+        trainable=True,
+        name=None,
+        reuse=None
+    )
 
 with tf.name_scope('FC_2'):
-    #act = fully_connected(act1,50, activation_fn=tf.nn.relu)
-    #wfc = tf.get_variable(name= 'w_FC_1', shape = [97,140,64], initializer=xavier_initializer(uniform=False, seed = seed), regularizer=l2_regularizer(regularization)) #size in 64 --> size out 5
-    #act = tf.scan(lambda x: tf.matmul(x,wfc, transpose_a=True), act1)
-    #flat = flatten(act3)
+    # act = fully_connected(act1,50, activation_fn=tf.nn.relu)
+    # wfc = tf.get_variable(name= 'w_FC_1', shape = [97,140,64], initializer=xavier_initializer(uniform=False, seed = seed), regularizer=l2_regularizer(regularization)) #size in 64 --> size out 5
+    # act = tf.scan(lambda x: tf.matmul(x,wfc, transpose_a=True), act1)
+    # flat = flatten(act3)
     activationFC2 = tf.layers.dense(
-    #tf.contrib.layers.maxout(activationFC1, num_units=20)
-    activationFC1,
-    features,
-    activation=None,
-    use_bias=False, #True
-    kernel_initializer=xavier_initializer(uniform=True, seed=seed),
-    bias_initializer=tf.constant_initializer(0.1),
-    kernel_regularizer=l2_regularizer(regularization),
-    bias_regularizer=None,
-    activity_regularizer=None,
-    trainable=True,
-    name=None,
-    reuse=None
-)
+        # tf.contrib.layers.maxout(activationFC1, num_units=20)
+        activationFC1,
+        features,
+        activation=None,
+        use_bias=False,  # True
+        kernel_initializer=xavier_initializer(uniform=True, seed=seed),
+        bias_initializer=tf.constant_initializer(0.1),
+        kernel_regularizer=l2_regularizer(regularization),
+        bias_regularizer=None,
+        activity_regularizer=None,
+        trainable=True,
+        name=None,
+        reuse=None
+    )
 
-
-cost = tf.reduce_mean(tf.nn.l2_loss(activationFC2-y))
+cost = tf.reduce_mean(tf.nn.l2_loss(activationFC2 - y))
 
 LR = tf.placeholder(tf.float32, [])
 
-
-
-
 train_step = tf.train.AdamOptimizer(learning_rate=LR).minimize(cost)
-
 
 merged = tf.summary.merge_all()
 
-
-
-        #tf.summary.histogram('FC-biases', bfc)
+# tf.summary.histogram('FC-biases', bfc)
 
 arr = []
 testarr = []
 
 saver = tf.train.Saver()
 
-
 with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer()) #*ONLY HERE ARE THE WEIGHTS INITIALIZED
-    a=0
+    sess.run(tf.global_variables_initializer())  # *ONLY HERE ARE THE WEIGHTS INITIALIZED
+    a = 0
     i_best = 1e5
     sTe, sTr = 1e8, 1e8  # placeholders
     costs = []  # placeholder
-    learning = 0.0145  #0001 TO DO: try with 3e-4, best learning rate
-    for i in range(100000): #20k
-        a+=1
+    learning = 0.002  # 0001 TO DO: try with 3e-4, best learning rate
+    for i in range(10000):  # 20k
+        a += 1
         input, answer = CT.getBatch()
         inputx = np.expand_dims(input, 3)
-        activation , output, costX, _ = sess.run([activationFC2, act8,  cost, train_step], feed_dict={y:answer, x:inputx, LR:learning, phase_train:True})
+        activation, output, costX, _ = sess.run([activationFC2, act8, cost, train_step],
+                                                feed_dict={y: answer, x: inputx, LR: learning, phase_train: True})
         tinput, tanswer = CT.getValBatch()
-        inferLocation, costTest = sess.run([activationFC2, cost], feed_dict={y:tanswer, x:np.expand_dims(tinput, 3), LR:learning, phase_train:False})
+        inferLocation, costTest = sess.run([activationFC2, cost],
+                                           feed_dict={y: tanswer, x: np.expand_dims(tinput, 3), LR: learning,
+                                                      phase_train: False})
 
         np.save(SaveTo + 'Costs_batch_train_test.npy', arr=costs)
-        if i %4000 == 0:
-        #if i%700 == 0:
-            learning *= 0.7
+        if i % 2000 == 0:
+            # if i%700 == 0:
+            learning *= 0.5
 
-        if costTest<sTe and costX < sTr and a>2500: #:
+        if costTest < sTe and a > 100:  #:
             sTe = costTest
             sTr = costX
-            i_best=i
-            print('APPENDED&SAVED')
-            saver.save(sess, SaveTo + 'BestModel.ckpt')
-            np.save(SaveTo + 'TrainBatch.npy', (activation, answer, costX))
-            np.save(SaveTo + 'TestSetResults.npy', (inferLocation, tanswer, costTest))
-            Y_pred = []
-            for d in range(X_pred.shape[0] + 1 - BatchSize): # ?
-                #stop  =  (i + 3) %140
-                inferLocation = sess.run([activationFC2],
-                                                   feed_dict={y: tanswer, x: np.expand_dims(X_pred[d:d+30], 3), LR: learning,
-                                                              phase_train: False})
-                Y_pred.append(inferLocation)
-            Y_pred = np.array(Y_pred)
-            np.save("C:/Y_pred.npy", Y_pred)
-            print("MODEL OUTPUT SHAPE: " + str(Y_pred.shape))
-            print("MODEL INPUT SHAPE: " + str(X_pred.shape))
-            print("PROCESSED output shape: " + str(CT.saveResults(Y_pred, SaveTo + 'Y_pred.npy').shape))
-            CT.saveResults(Y_pred, SaveTo + "/y_pred/Y_pred{}.npy".format(i))
+            i_best = i
+            print('APPENDED&SAVED ' + str(costTest) + " trainC: " + str(costX))
+            os.makedirs(SaveTo+str(costX) + "testcost is "+ str(costTest) +"/")
+            saver.save(sess, SaveTo+str(costX) + "testcost is "+ str(costTest) +"/" + 'BestModel.ckpt', global_step=i)
+            np.save(SaveTo+str(costX) + "testcost is "+ str(costTest) +"/" + 'TrainBatch.npy', (activation, answer, costX))
+            np.save(SaveTo+str(costX) + "testcost is "+ str(costTest) +"/" + 'ValSetResults.npy', (inferLocation, tanswer, costTest))
+            # TODO:
+            # Y_pred = []
+            # for d in range(X_pred.shape[0] + 1 - BatchSize):  # ?
+            #     # stop  =  (i + 3) %140
+            #     inferLocation = sess.run([activationFC2],
+            #                              feed_dict={y: tanswer, x: np.expand_dims(X_pred[d:d + BatchSize], 3), LR: learning,
+            #                                         phase_train: False})
+            #     Y_pred.append(inferLocation)
+            # Y_pred = np.array(Y_pred)
+            # np.save("C:/Y_pred.npy", Y_pred)
+            # print("MODEL OUTPUT SHAPE: " + str(Y_pred.shape))
+            # print("MODEL INPUT SHAPE: " + str(X_pred.shape))
+            # print("PROCESSED output shape: " + str(CT.saveResults(Y_pred, SaveTo + 'Y_pred.npy').shape))
+            # CT.saveResults(Y_pred, SaveTo + "/y_pred/Y_pred{}.npy".format(i))
 
-
-
-        if(i%50==0):
+        if (i % 50 == 0):
             print('{} TRAINCost: {} TESTCost: {} LR: {}'.format(i, costX, costTest, learning))
             costs.append([costX, costTest])
-        if(i_best+10000<i):
+        if (i_best + 10000 < i):
             print("overfitting detected, breaking the loop")
             break
 
-
-    #writer.
+    # writer.
 """bla = np.array(output) ???
 print(output.shape)
 print(bla.shape)
@@ -290,6 +284,3 @@ bla = bla[4]
 #bla = np.squeeze(bla, axis = 2)
 plt.imshow(bla)
 plt.show()"""
-
-
-
